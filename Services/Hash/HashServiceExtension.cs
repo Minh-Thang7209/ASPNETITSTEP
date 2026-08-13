5e29f8b729bb0c349d0ba40b5fc6f4d6e025998f
Your Name <your.email@example.com>
2026-08-13 19:29:48 +0300

    add code lesson

--- Services/Hash/HashServiceExtension.cs
+++ Services/Hash/HashServiceExtension.cs
@@ -6,6 +6,10 @@ namespace ASPNETITSTEP.Services.Hash
             this IServiceCollection services)
         {
             return services.AddSingleton<IHashService, Md5HashService>();
+            // return services.AddScoped<IHashService, Md5HashService>();
+            // return services.AddTransient<IHashService, Md5HashService>();
         }
     }
 }
+
+// Сервіс можна зареєструвати 3 класичними спосабами 
--- obj/Debug/net10.0/ASPNETITSTEP.AssemblyInfo.cs
+++ obj/Debug/net10.0/ASPNETITSTEP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPNETITSTEP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aa2bcf6576ca8d3ca6dcf6602ecab19107dacc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1608dda8789c9cb068b2440cd2851ccc741e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPNETITSTEP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPNETITSTEP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
