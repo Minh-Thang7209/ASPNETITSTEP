8cfe97ac974c37514e7749bebfbe17c5c6b0cd5e
Thang <minhthang7209@gmail.com>
2026-08-16 19:53:38 +0300

    add hw code

--- Models/Home/Models/HomeModelsFormModel.cs
+++ Models/Home/Models/HomeModelsFormModel.cs
@@ -9,6 +9,21 @@ namespace ASPNETITSTEP.Models.Home.Models
 
         [FromForm(Name = "user-password")]
         public String UserPassword { get; set; } = null!;
+
+        [FromForm(Name = "user-agree")]
+        public bool UserAgree { get; set; }
+
+        [FromForm(Name = "user-gender")]
+        public String UserGender { get; set; } = null!;
+
+        [FromForm(Name = "user-birthdate")]
+        public DateTime UserBirthdate { get; set; }
+
+        [FromForm(Name = "user-color")]
+        public string UserColor { get; set; } = null!;
+
+        [FromForm(Name = "user-age")]
+        public int UserAge { get; set; }
     }
 }
 
