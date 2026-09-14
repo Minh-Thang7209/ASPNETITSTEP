e2affca154d5911e4352f24cd56f28a27b28c0e5
Thang <minhthang7209@gmail.com>
2026-09-14 22:06:38 +0300

    Saves

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,6 +2,7 @@ using ASPNETITSTEP.Data;
 using ASPNETITSTEP.Migrations;
 using ASPNETITSTEP.Models.Admin;
 using ASPNETITSTEP.Services.Storage;
+using ASPNETITSTEP.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_P42.Controllers
@@ -26,79 +27,80 @@ namespace ASP_P42.Controllers
             return View(viewModel);
         }
 
-        // [HttpPost]
-        // public IActionResult AddProduct(AdminAddProductFormModel formModel)
-        // {
-        //     try
-        //     {
-        //         /* Д.З. Реалізувати валідацію моделі форми 
-        //          * додавання нового товару
-        //          * - назва (довжина, відсутність спецсимволів)
-        //          * - опис - якщо передається, то перевіряється (довжина), інакше ігнорується
-        //          * - Slug (унікальність, url-коректність) - якщо передається
-        //          * - кількість - ціле позитивне число або "-1"
-        //          * - ціна - позитивне число більше за 0.01
-        //          */
-        //         // 
-        //         // оскільки товари МАЮТЬ належати певній групі, перевіряємо її правильність
-        //         Data.Entities.ProductGroup group = _dataContext
-        //             .ProductGroups
-        //             .FirstOrDefault(g => g.Id == formModel.GroupId)
-        //         ?? throw new Exception($"Group not found with id='{formModel.GroupId}'");
-
-        //         if (formModel.ProductId != null)
-        //         {
-        //             // додавання нової версії, слід пересвідчитись у наявності товару
-        //             Data.Entities.Product product = _dataContext
-        //                 .Products
-        //                 .FirstOrDefault(p => p.Id == formModel.ProductId) 
-        //             ?? throw new Exception($"Product not found with id='{formModel.ProductId}'");
+        [HttpPost]
+        public IActionResult AddProduct(AdminAddProductFormModel formModel)
+        {
+            try
+            {
+                /* Д.З. Реалізувати валідацію моделі форми 
+                 * додавання нового товару
+                 * - назва (довжина, відсутність спецсимволів)
+                 * - опис - якщо передається, то перевіряється (довжина), інакше ігнорується
+                 * - Slug (унікальність, url-коректність) - якщо передається
+                 * - кількість - ціле позитивне число або "-1"
+                 * - ціна - позитивне число більше за 0.01
+                 */
+                // 
+                // оскільки товари МАЮТЬ належати певній групі, перевіряємо її правильність
+                ProductGroup group = _dataContext
+                    .ProductGroups
+                    .FirstOrDefault(g => g.Id == formModel.GroupId)
+                ?? throw new Exception($"Group not found with id='{formModel.GroupId}'");
 
 
-        //         }
-        //         else
-        //         {
-        //             // додавання нового товару
-        //             // оскільки зображення (картинка) опціональна, перевіряємо без винятків
-        //             String? imageUrl = null;
-        //             if(formModel.Image != null)
-        //             {
-        //                 // але якщо дані передано, то перевіряємо повністю
-        //                 imageUrl = _storageService.Save(formModel.Image);
-        //             }
-        //             // Розбираємо дані на Товар і Версію
-        //             Guid productId = Guid.NewGuid();
-        //             _dataContext.Products.Add(new()
-        //             {
-        //                 Id = productId,
-        //                 GroupId = group.Id,
-        //                 Name = formModel.Name,
-        //                 Description = formModel.Description,
-        //                 ImageUrl = imageUrl,
-        //                 IsHidden = formModel.IsHidden,
-        //                 OrderInPrice = formModel.Order,
-        //                 Slug = formModel.Slug,
-        //             });
-        //             _dataContext.ProductVersions.Add(new()
-        //             {
-        //                 Id = Guid.NewGuid(),
-        //                 ProductId = productId,
-        //                 ImageUrl = imageUrl,
-        //                 Price = (decimal)formModel.Price,
-        //                 Stock = formModel.Stock,
-        //                 OrderInPrice = 1,
-        //                 Slug = formModel.Slug,
-        //                 IsHidden = formModel.IsHidden,                        
-        //             });
-        //             _dataContext.SaveChanges();
-        //         }
-        //         return Ok();
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         return BadRequest(ex.Message);
-        //     }            
-        // }
+                if (formModel.ProductId != null)
+                {
+                    // додавання нової версії, слід пересвідчитись у наявності товару
+                    Product product = _dataContext
+                        .Products
+                        .FirstOrDefault(p => p.Id == formModel.ProductId) 
+                    ?? throw new Exception($"Product not found with id='{formModel.ProductId}'");
+
+
+                }
+                else
+                {
+                    // додавання нового товару
+                    // оскільки зображення (картинка) опціональна, перевіряємо без винятків
+                    String? imageUrl = null;
+                    if(formModel.Image != null)
+                    {
+                        // але якщо дані передано, то перевіряємо повністю
+                        imageUrl = _storageService.Save(formModel.Image);
+                    }
+                    // Розбираємо дані на Товар і Версію
+                    Guid productId = Guid.NewGuid();
+                    _dataContext.Products.Add(new()
+                    {
+                        Id = productId,
+                        GroupId = group.Id,
+                        Name = formModel.Name,
+                        Description = formModel.Description,
+                        ImageUrl = imageUrl,
+                        IsHidden = formModel.IsHidden,
+                        OrderInPrice = formModel.Order,
+                        Slug = formModel.Slug,
+                    });
+                    _dataContext.ProductVersions.Add(new()
+                    {
+                        Id = Guid.NewGuid(),
+                        ProductId = productId,
+                        ImageUrl = imageUrl,
+                        Price = (decimal)formModel.Price,
+                        Stock = formModel.Stock,
+                        OrderInPrice = 1,
+                        Slug = formModel.Slug,
+                        IsHidden = formModel.IsHidden,                        
+                    });
+                    _dataContext.SaveChanges();
+                }
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }            
+        }
 
         public IActionResult Group()
         {
---
+++ Migrations/20260914185726_Saves.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPNETITSTEP.Migrations
+{
+    /// <inheritdoc />
+    public partial class Saves : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAccesses_UsersRoles_RoleId",
+                table: "UserAccesses");
+
+            migrationBuilder.DropTable(
+                name: "AuthJournals");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UsersRoles",
+                table: "UsersRoles");
+
+            migrationBuilder.RenameTable(
+                name: "UsersRoles",
+                newName: "UserRoles");
+
+            migrationBuilder.RenameColumn(
+                name: "BirthDate",
+                table: "UsersData",
+                newName: "Birthdate");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderInPrice",
+                table: "ProductVersions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderInPrice",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles",
+                column: "Id");
+
+            migrationBuilder.UpdateData(
+                table: "UserAccesses",
+                keyColumn: "Id",
+                keyValue: new Guid("96dcbbba-9aee-44a2-8835-72dfe4e1a710"),
+                column: "RoleId",
+                value: new Guid("acb35324-7b84-4e3b-9a26-00aad72a600c"));
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("702d05c2-fcd0-4c1d-b0bb-2aeb4b98f91a"),
+                column: "Description",
+                value: "Самозареєстрований користувач");
+
+            migrationBuilder.UpdateData(
+                table: "UsersData",
+                keyColumn: "Id",
+                keyValue: new Guid("190052ca-f844-498a-a05f-1d4ba2adc0e8"),
+                column: "FullName",
+                value: "Адміністратор Системи");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAccesses_UserRoles_RoleId",
+                table: "UserAccesses",
+                column: "RoleId",
+                principalTable: "UserRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAccesses_UserRoles_RoleId",
+                table: "UserAccesses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "OrderInPrice",
+                table: "ProductVersions");
+
+            migrationBuilder.DropColumn(
+                name: "OrderInPrice",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                newName: "UsersRoles");
+
+            migrationBuilder.RenameColumn(
+                name: "Birthdate",
+                table: "UsersData",
+                newName: "BirthDate");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UsersRoles",
+                table: "UsersRoles",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "AuthJournals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Dk = table.Column<string>(type: "TEXT", nullable: false),
+                    IsOk = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Login = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthJournals", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "UserAccesses",
+                keyColumn: "Id",
+                keyValue: new Guid("96dcbbba-9aee-44a2-8835-72dfe4e1a710"),
+                column: "RoleId",
+                value: new Guid("702d05c2-fcd0-4c1d-b0bb-2aeb4b98f91a"));
+
+            migrationBuilder.UpdateData(
+                table: "UsersData",
+                keyColumn: "Id",
+                keyValue: new Guid("190052ca-f844-498a-a05f-1d4ba2adc0e8"),
+                column: "FullName",
+                value: "Адміністратор системи");
+
+            migrationBuilder.UpdateData(
+                table: "UsersRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("702d05c2-fcd0-4c1d-b0bb-2aeb4b98f91a"),
+                column: "Description",
+                value: "Самозаєстрований користувач");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAccesses_UsersRoles_RoleId",
+                table: "UserAccesses",
+                column: "RoleId",
+                principalTable: "UsersRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
